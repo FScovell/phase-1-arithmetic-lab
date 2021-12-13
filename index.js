e054f2def3ad5ad9dca169eeb7d41068c8568925
Frederick <admin@freddyscovell.com>
2021-12-13 18:10:49 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
-// Write your code here
+/*function multiply(num1, num2){
+    let solvedM = num1*num2;
+    return solvedM;
+}
+
+multiply(31, 2);
+*/
+let num1 = 31;
+let num2 = 2;
+const multiply = (num1*num2);
+
+
+function randomizer(){
+    let pandom = Math.floor(Math.random() * 10) + 1;
+    return pandom;
+}
+
+let random = randomizer();
+
+let num3 = 8;
+let num4 = 2;
+const mod = (num3/num4);
+
+const max = Math.max(5, 20);
